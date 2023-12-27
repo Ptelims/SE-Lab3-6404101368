42cd0be8f3b29bad13c12689c1c0ca94eb5ac41c
Ptelims <smile1500903198@gmail.com>
2023-12-27 16:15:36 +0000

    name lastname id date

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -17,6 +17,15 @@
 
       <q-input
         filled
+        v-model="name"
+        label="Your Name *"
+        hint="First name and surname"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type something']"
+      />
+
+      <q-input
+        filled
         type="number"
         v-model="age"
         label="你的年齡 *"
